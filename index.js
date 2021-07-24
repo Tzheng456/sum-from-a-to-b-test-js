d17b8e6b94a01a5193b0c610aa15f0f78279b4d7
Tzheng456 <Tzheng456@gmail.com>
2021-07-24 01:57:20 +0000

    implemented sum in index.js

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-
 function sum(fromN, toN) {
   // Sum all the values from fromN up to toN
+  if (fromN === toN) {
+    return fromN;
+  }
+  return fromN + sum(++fromN, toN);
 }
 
 module.exports = sum;
